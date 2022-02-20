bb503881757d09262e3aa9b52621bde484d3dca9
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2022-02-19 19:03:29 -0600

    Update exercise-1-4.rb

--- part-1-exercises/exercise-1-4.rb
+++ part-1-exercises/exercise-1-4.rb
@@ -1,10 +1,8 @@
 
+
 def names_of_my_pets (name_1, name_2, name_3)
   puts "I have 3 pets. Their names are #{name_1}, #{name_2}, and #{name_3}"
 end
 
-name_1 = "Apollo"
-name_2 = "Athena"
-name_3 = "Loki"
 
-names_of_my_pets (name_1, name_2, name_3)
+names_of_my_pets("Esme", "Albus", "Aberforth")
