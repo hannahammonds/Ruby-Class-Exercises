eff5e93bb24149fb50d1ee5298a28e58b73d691b
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2022-02-19 19:13:25 -0600

    Update exercise-1-5.rb

--- part-1-exercises/exercise-1-5.rb
+++ part-1-exercises/exercise-1-5.rb
@@ -1,10 +1,9 @@
 sentence = "Hello John Doe!"
 
-new_sentence = sentence.sub("Hello", "Hi")
+new_sentence = sentence.gsub("Hello", "Hi")
 
 puts new_sentence
 
 solution = "Hi John Doe!"
-puts solution.match?(/new_sentence/)
-
+puts solution.match?(new_sentence)
 
